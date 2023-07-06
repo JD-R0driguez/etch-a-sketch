f1bdc925318e5a75dfedbbb6c7ef7434b6cf8940
Juan <jd.rodriguez.or@gmail.com>
2023-07-06 13:30:36 -0400

    Complete grid sizing functionality

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
+const DefaultRows= 16;
+const DefaultColumns= 16;
 
 const drawingGrid =  document.getElementById('draw-area');
+const gridSizeSlider = document.getElementById('range-slider');
+const gridSizeText = document.getElementById('size-value'); 
 
-drawingGrid.innerHTML = '';
-
-let gridRows = 10;
-let gridColumns = 10;
-let gridMatrix =  gridRows * gridColumns;
-
-for(let i = 0; i < gridMatrix; i++){
-    const gridPixel = document.createElement('div');
-    gridPixel.classList.add('grid-pixel');
-    drawingGrid.appendChild(gridPixel);
-}
-
-drawingGrid.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`;
-drawingGrid.style.gridTemplateRows = `repeat(${gridRows}, 1fr)`;
+createDrawingMatrix(DefaultRows, DefaultColumns);
 
 drawingGrid.addEventListener('mouseover', function(event) {
     if (event.target.classList.contains('grid-pixel')) {
@@ -22,14 +13,39 @@ drawingGrid.addEventListener('mouseover', function(event) {
     }
 });
 
+gridSizeSlider.addEventListener('input', function(){
+    const newSize = this.value;
+    gridSizeText.innerHTML = `${newSize} x ${newSize}`;
+    createDrawingMatrix(newSize, newSize);
+})
 
-//Event handler for whe the mouse leaves the pixel
+function createDrawingMatrix (numRows, numColumns){
+    
+    drawingGrid.innerHTML = '';
+    const gridMatrix =  numRows * numColumns;
+    
+    for(let i = 0; i < gridMatrix; i++){
+        const gridPixel = document.createElement('div');
+        gridPixel.classList.add('grid-pixel');
+        drawingGrid.appendChild(gridPixel);
+    }
+    drawingGrid.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
+    drawingGrid.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
 
-// drawingGrid.addEventListener('mouseout', function(event) {
-//     if (event.target.classList.contains('grid-pixel')) {
-//       event.target.style.backgroundColor = ''; // Reset the background color
-//     }
-// });
+    // alert(gridSizeSlider.value);
+
+}
+
+
+
+/* //Event handler for whe the mouse leaves the pixel
+
+drawingGrid.addEventListener('mouseout', function(event) {
+    if (event.target.classList.contains('grid-pixel')) {
+      event.target.style.backgroundColor = ''; // Reset the background color
+    }
+});
+*/
 
 
 
